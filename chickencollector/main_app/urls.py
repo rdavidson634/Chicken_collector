ad75d7f596ff1d976d474f16a3666f04a1e795b7
rdavidson634 <rdavidson634@gmail.com>
2020-09-16 13:45:45 -0600

    done

---
+++ chickencollector/main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('chickens/', views.chickens_index, name='index'),
+    path('chickens/<int:chicken_id>/', views.chickens_detail, name='detail')
+]
---
+++ chickencollector/main_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Chicken
+
+# Create your views here.
+def home (request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def chickens_index(request):
+    chickens = Chicken.objects.all()
+    return render(request, 'chickens/index.html', {'chickens': chickens})
+
+def chickens_detail(request, chicken_id):
+    chicken = Chicken.objects.get(id=chicken_id)
+    return render(request, 'chickens/detail.html', {'chicken': chicken})
